e725e79ef13f4f39011c983f46349fc5cc90d2c4
Max <franzmaximilian1996@gmail.com>
2018-08-23 11:49:57 +0200

    remove obsolete test_db.py file

--- test_db.py
+++
@@ -1,6 +0,0 @@
-from restapi.models import Member, SpecificDate
-from restapi.serializer import MemberSerializer, SpecificDateSerializer
-
-Member.objects.create(first_name="max", last_name="franz")
-
-
