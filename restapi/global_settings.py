43b39ea9397a5f4db910af0eacec09ccc9b67944
Max <franzmaximilian1996@gmail.com>
2018-08-31 09:50:11 +0200

    add global settings and remove department endpoint from urls

--- restapi/global_settings.py
+++ restapi/global_settings.py
@@ -6,6 +6,8 @@ Contains
 
 """
 
+import datetime
+
 # ----------------------------------
 # Validation Error Message
 # ----------------------------------
@@ -23,3 +25,45 @@ DATE_NOT_ON_WEEKDAY = 'Date is not on the weekday of this course'
 TOO_MANY_SUPERVISORS_FREE_TRAINING = 'Only one supervisor per Free-Training is allowed'
 
 START_AFTER_END = 'Start Date cannot be after the End Date for a subscription'
+
+# ----------------------------------
+# Meaningful Default Model Variables
+# ----------------------------------
+
+# Cost for attending a course without subscription (i.e. free training)
+DEFAULT_ATTENDANCE_COST = 3
+
+DEFAULT_WAGE = 15
+DEFAULT_SECONDARY_WAGE = 10
+
+DEFAULT_START_TIME = datetime.time(16, 00, 00)
+DEFAULT_END_TIME = datetime.time(17, 30, 00)
+DEFAULT_TIME_IN_HOURS = 1.5
+
+
+
+
+# ----------------------------------
+# Choices
+# ----------------------------------
+
+DEPARTMENTS = ((0, 'Parkour'),
+               (1, 'Tricking'),
+               )
+
+DAYS_OF_WEEK = ((0, 'Monday'),
+                (1, 'Tuesday'),
+                (2, 'Wednesday'),
+                (3, 'Thursday'),
+                (4, 'Friday'),
+                (5, 'Saturday'),
+                (6, 'Sunday')
+                )
+
+EVENT_TYPES = ((0, 'Course'),
+               (1, 'Free Training'))
+
+ATTENDANCE_STATUS = ((0, 'not specified'),
+                     (1, 'excused'),
+                     (2, 'attended'),
+                     (3, 'not attended'))
