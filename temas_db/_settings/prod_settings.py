76ce3b2be6ba248b2710e1ed250a2cacf0572249
Max Franz <franzmaximilian1996@gmail.com>
2018-09-22 12:34:44 +0000

    fix relative path

--- temas_db/_settings/prod_settings.py
+++ temas_db/_settings/prod_settings.py
@@ -9,7 +9,7 @@ import os
 import raven
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.join(os.path.abspath(__file__), '..'))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 with open('secret.txt') as f:
